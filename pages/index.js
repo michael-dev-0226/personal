587692220528c6bb21bf242f3fde921760292a58
michael <solomon226@hotmail.com>
2022-09-28 23:09:48 +0800

    Update the cotent of home page

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,12 @@ import {
   Icon,
   Link,
   Divider,
+  Code,
 } from '@chakra-ui/core'
 
 import Timeline from '../components/timeline'
 import Layout from '../components/layout'
+import ProjectCard from '../components/projectCard'
 
 const Index = () => {
   const { colorMode } = useColorMode()
@@ -32,6 +34,10 @@ const Index = () => {
     light: 'black',
     dark: 'green.100',
   }
+  // const blueTextColor = {
+  //   light: 'black',
+  //   dark: 'blue.200',
+  // }
 
   const purpleColor = {
     light: 'purple.600',
@@ -65,13 +71,18 @@ const Index = () => {
             as="h1"
             size="2xl"
           >
-            👋 Hello, I’m Yuki!
+            👋 Hello, I’m Michael!
           </Heading>
           <Text mb={4} color={secondaryTextColor[colorMode]}>
+            I'm a Singaporean maker over paying rent in Laguna Hills, California
+            🇸🇬🇺🇸. In my day job, I mostly run up and down the software stack as
+            a fullstack engineer (<i>with a focus on frontend engineering</i>).
+          </Text>
+          <Text mb={4} color={secondaryTextColor[colorMode]}>
             I enjoy feature development and working on products that matter! Now
-            that I've been curious about the blockchain technology and traing my
-            skills for future instustry. I wish I knew while working in this
-            industry.
+            that I've been working for a couple of years, I decided to create
+            this blog to jot down things I wish I knew while working in this
+            industry:
             <Box as="ul" pt={2} pl={4} ml={2}>
               <Box as="li" pb={1}>
                 Soft skills
@@ -96,7 +107,68 @@ const Index = () => {
             </Link>
             ; I enjoy chatting with folk.
           </Text>
+          <Code>Hi This is code</Code>
         </Flex>
+        {/*<Box w="100%">*/}
+        {/*  <Divider />*/}
+        {/*  <Flex w="100%" justifyContent="space-evenly" alignItems="center">*/}
+        {/*    <Icon*/}
+        {/*      aria-label={'periscopedata'}*/}
+        {/*      name={'periscopedata'}*/}
+        {/*      color={purpleColor[colorMode]}*/}
+        {/*      size={['50px', '50px', '60px']}*/}
+        {/*      ml={2}*/}
+        {/*      mr={4}*/}
+        {/*    />*/}
+        {/*    <Icon*/}
+        {/*      aria-label={'apple'}*/}
+        {/*      name={'apple'}*/}
+        {/*      color={grayColor[colorMode]}*/}
+        {/*      size={['50px', '50px', '60px']}*/}
+        {/*      ml={2}*/}
+        {/*      mr={4}*/}
+        {/*    />*/}
+        {/*    <Icon*/}
+        {/*      aria-label={'sisense'}*/}
+        {/*      name={'sisense'}*/}
+        {/*      size={['50px', '50px', '60px']}*/}
+        {/*      ml={2}*/}
+        {/*      mr={4}*/}
+        {/*    />*/}
+        {/*    <Icon*/}
+        {/*      aria-label={'crowdai'}*/}
+        {/*      name={'crowdai'}*/}
+        {/*      size={['75px', '75px', '100px']}*/}
+        {/*      ml={2}*/}
+        {/*      mr={4}*/}
+        {/*    />*/}
+        {/*  </Flex>*/}
+        {/*  <Divider />*/}
+        {/*</Box>*/}
+        {/*<Flex*/}
+        {/*  flexDirection="column"*/}
+        {/*  justifyContent="flex-start"*/}
+        {/*  alignItems="flex-start"*/}
+        {/*  maxWidth="800px"*/}
+        {/*>*/}
+        {/*  <Heading*/}
+        {/*    color={yellowTextColor[colorMode]}*/}
+        {/*    letterSpacing="tight"*/}
+        {/*    mb={4}*/}
+        {/*    size="xl"*/}
+        {/*    fontWeight={700}*/}
+        {/*  >*/}
+        {/*    Project*/}
+        {/*  </Heading>*/}
+        {/*  <ProjectCard*/}
+        {/*    title="Objective: Object Collective Tarot Deck"*/}
+        {/*    description="An illustration project to reimagine the Rider-Waite tarot deck as relatable items/animals in the 21st century"*/}
+        {/*    href="https://www.kickstarter.com/projects/1988162704/objective-object-collective-tarot-deck"*/}
+        {/*    icon="objective"*/}
+        {/*    size="56px"*/}
+        {/*  />*/}
+        {/*</Flex>*/}
+
         <Flex
           flexDirection="column"
           justifyContent="flex-start"
@@ -117,11 +189,12 @@ const Index = () => {
             activitiesByYear={{
               '2022': [
                 {
-                  title: 'Freelancing for Newwit',
+                  title: 'Joined Gelato Network',
                   description:
-                    'I jumped into smart contracts development which was my short-term desire and developed governance and unitiy tokens and built bridged connecting EVM chains',
+                    "With a leap of faith, I jumped into a seed stage startup to flex my engineering muscle; bringing a tool to build automation into dapps to the table. Here I got to build the company's vision of it's product..",
                   tags: {
-                    warehouse_tab: 'https://twitter.com/NewwitApp',
+                    warehouse_tab:
+                      'https://www.youtube.com/channel/UCBk9gXYKOoLHTQxpcnZkBwQ',
                   },
                 },
               ],
@@ -129,10 +202,19 @@ const Index = () => {
                 {
                   title: 'Joined Onessus',
                   description:
-                    'In the end of 2021, I jumped into a NFT platform Development Studio; brought full-stack engineering to the table. Here I got to transfer novel concept to reality.',
+                    'In the end of 2021, I jumped into a dApp Development Studio; brought full-stack engineering to the table. Here I got to transfer novel concept to reality.',
                   tags: {
                     warehouse_tab:
                       'https://www.youtube.com/channel/UCyZaZqRgYFdrgVFoB-qT-ag',
+                  },
+                },
+                {
+                  title: 'Joined Wallfair.io',
+                  description:
+                    'In Sep 2021, Took my second frontend engineering to a decentralized social entertainment industry: Here I got to build the frontend pages of social events.',
+                  tags: {
+                    warehouse_tab:
+                      'https://www.youtube.com/channel/UCqBlRN4MA8N2-SNvM5V52Eg',
                   },
                 },
                 {
@@ -147,29 +229,29 @@ const Index = () => {
               ],
               '2020': [
                 {
-                  title: 'Joined Maryoku',
-                  description:
-                    'I have experience large-scale and ambious product development',
-                  tags: {
-                    warehouse_tab: 'https://www.linkedin.com/company/262days/',
-                  },
+                  title: 'Joined Snikpic',
+                  description: 'My career landed on more variety fields',
                 },
               ],
               '2019': [
                 {
-                  title: 'Joined Common-Services',
+                  title: 'Joined Demoreel.com',
                   description:
-                    'Bring the product in the Shopify App marketplace',
-                  tags: {
-                    warehouse_tab: 'https://apps.shopify.com/amazon-3',
-                  },
+                    'I started freelancing and experienced many sources and libraries.',
                 },
               ],
               '2016': [
                 {
-                  title: 'Joined Style Inc',
+                  title: 'Switch to Computer Engineering Major',
                   description:
-                    'Engaged in CV matching, Company internal program development.',
+                    "I've tried mechanical, eletrical and chemical engineering and at this point nothing really stood out. I went back to software engineering (building something by myself from scratch was fun).",
+                },
+              ],
+              '2014': [
+                {
+                  title: 'Took my first CS elective',
+                  description:
+                    'I scored a 20 out of 100 in the finals; and thought coding was not for me.',
                 },
               ],
             }}
